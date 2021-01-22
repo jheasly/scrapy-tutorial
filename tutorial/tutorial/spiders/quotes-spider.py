59d119c567774715c48380bedba074ad2a46d70a
J. Heasly <jheasly@earthlink.net>
2021-01-22 15:32:32 -0600

    First commit.

---
+++ tutorial/tutorial/spiders/quotes-spider.py
@@ -0,0 +1,19 @@
+# https://healthspace.com/clients/oregon/state/statewebportal.nsf/module_healthRegions.xsp
+
+import scrapy
+
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+
+    start_urls = ['http://quotes.toscrape.com/']
+    start_urls = ['https://healthspace.com/Clients/Oregon/Lane/Web.nsf/module_facilities.xsp?module=Food']
+
+    def parse(self, response):
+        self.logger.info('hello this is my first spider')
+        establishments = response.css('td')
+        for establishment in establishments:
+            yield {
+                'name': establishment.css('.hidden-xs::text').get(),
+            }
+
+# response.css('td').css('.hidden-xs::text').get()
